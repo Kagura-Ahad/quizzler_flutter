ffaf69119e78b207aafd4d02998ddfdc6bf00035
Ahad <sa07753@st.habib.edu.pk>
2023-06-09 22:18:11 +0500

    Initial commit

---
+++ lib/questionClass.dart
@@ -0,0 +1,6 @@
+class Question {
+  late String questionText; // Field to hold the question text
+  late bool questionAnswer; // Field to hold the answer to the question
+
+  Question( this.questionText, this.questionAnswer);
+}
